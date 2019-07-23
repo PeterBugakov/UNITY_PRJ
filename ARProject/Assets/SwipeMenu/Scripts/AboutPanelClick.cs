297b283ea1fc152e4d48f9b76222fc37d8ce9231
PeterBugakov <52630618+PeterBugakov@users.noreply.github.com>
2019-07-23 23:30:27 +0700

    MainMenu v0.2 alpha-testing

---
+++ ARProject/Assets/SwipeMenu/Scripts/AboutPanelClick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwipeMenu;
+
+public class AboutPanelClick : MonoBehaviour
+{
+    private int clk=0;
+    private void Start()
+    {
+        Panel.gameObject.SetActive(false);
+        GameObject MainMenu = GameObject.Find("Menu");
+        MainMenu.GetComponent<Menu>().ShowMenus();
+    }
+
+    public GameObject Panel;
+    public void HidePanel()
+    {
+        Panel.gameObject.SetActive(false);
+        GameObject MainMenu = GameObject.Find("Menu");
+        MainMenu.GetComponent<Menu>().ShowMenus();
+        
+    }
+    public void ShowPanel()
+    {
+        clk++;
+        GameObject MainMenu = GameObject.Find("Menu");
+        if (clk==1)
+        {
+            Panel.gameObject.SetActive(true);
+            MainMenu.GetComponent<Menu>().HideMenus();
+        }
+        if (clk>=2)
+        {
+            Panel.gameObject.SetActive(false);
+            MainMenu.GetComponent<Menu>().ShowMenus();
+            clk = 0;
+        }
+       /* GameObject txt;
+        txt = GameObject.Find("City");
+        txt.GetComponent<TMPro.TextMeshProUGUI>().text=clk.ToString();*/
+}
+}
+
+
---
+++ ARProject/Assets/SwipeMenu/Scripts/SwipeMenu/UpdateNameMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using SwipeMenu;
+
+    /// <summary>
+    /// Выводит название выбранного пункта меню
+    /// </summary>
+    public class UpdateNameMenu : MonoBehaviour
+    {
+        public TMPro.TextMeshProUGUI txt;
+
+        public void Update()
+        {
+            string Mname;
+            Mname = Menu.instance.menuItems[Menu.instance.GetClosestMenuItemIndex()].name;
+            if (Mname == "Food") Mname = "объекты питания";
+            if (Mname == "Medicine") Mname = "медицина";
+            if (Mname == "Hotels") Mname = "гостиницы";
+            if (Mname == "Places") Mname = "достопримечательности";
+            if (Mname == "Routes") Mname = "пешеходные маршруты";
+            if (Mname == "Museums") Mname = "культура";
+            txt.text = Mname;
+        }
+    }
+
